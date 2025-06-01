b52a695340f6c69d83b48f553482fb11af1deba6
Redang7 <joaovpraddo12@gmail.com>
2025-06-01 10:38:46 -0300

    Palindrome-minmax

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(str) {
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let invertido = str.split('').reverse().join('');
+    return str === invertido;
 }
-
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+function arrayMaxMin(arr) {
+    let min = arr[0];
+    let max = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return [min, max];
 }
